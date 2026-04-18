b5af0a59bcd593b800122c1555b6a058f5770e17
David E. Wheeler <david.wheeler@clickhouse.com>
2026-04-18 13:26:51 -0400

    Add failing streaming test
    Set the fetch size to just one byte, then have it fetch 3500 integers.
    At a certain point, it fails with
    
        ERROR:  invalid input syntax for type integer: "Host: 127.0.0.1:8123
    
    Seems like somehow a header is fed into the fetch routine. Seen on macOS
    with libcurl 8.19.0.

---
+++ test/sql/stream_out.sql
@@ -0,0 +1,19 @@
+CREATE SERVER try_http FOREIGN DATA WRAPPER clickhouse_fdw
+    OPTIONS(dbname 'try_test', driver 'http', fetch_size '1');
+CREATE USER MAPPING FOR CURRENT_USER SERVER try_http;
+
+SELECT clickhouse_raw_query('DROP DATABASE IF EXISTS try_test');
+SELECT clickhouse_raw_query('CREATE DATABASE try_test');
+SELECT clickhouse_raw_query('CREATE TABLE try_test.t1 (c1 Int32)
+    ENGINE = MergeTree ORDER BY c1');
+SELECT clickhouse_raw_query('INSERT INTO try_test.t1
+    SELECT number FROM numbers(3500)');
+
+CREATE FOREIGN TABLE try_http_ft (c1 int)
+    SERVER try_http OPTIONS (table_name 't1');
+
+SELECT * FROM try_http_ft;
+
+DROP USER MAPPING FOR CURRENT_USER SERVER try_http;
+DROP SERVER try_http CASCADE;
+SELECT clickhouse_raw_query('DROP DATABASE try_test');
